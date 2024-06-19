f42cfecf6bda12ceef144d5a71eb8c95a2e6422a
jeevan-aj <ambrahamjeevan@gmail.com>
2024-06-20 00:29:57 +0530

    backend and frontend Functionalities done

--- Seat-Booking/src/Redux/InitialSlice.jsx
+++ Seat-Booking/src/Redux/InitialSlice.jsx
@@ -1,18 +1,43 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import dayjs from "dayjs";
 
 const initialState = {
-    value: 0,
-  }
-
-  export const InitialSlice = createSlice({
-    name: 'initialSice',
-    initialState,
-    reducers: {
-        increment:(state,action) => {
-            state.value += action.payload
-        }
+  curentDate: dayjs(),
+  selectedSeat: null,
+  bookings: [
+    { id: 1, seatNo: 2, BookingDate: dayjs(), BookedBy: "jeevan" },
+    {
+      id: 5,
+      seatNo: 5,
+      BookingDate: dayjs().add(1, "Day"),
+      BookedBy: "jeevan",
     },
-  })
+  ],
+  isOpen: false,
+  isCurrentUser:false
+};
 
-  export const { increment } = InitialSlice.actions
-  export default InitialSlice.reducer
+export const InitialSlice = createSlice({
+  name: "initialSlice",
+  initialState,
+  reducers: {
+    setCurrentDate: (state, action) => {
+      state.curentDate = action.payload;
+    },
+    setSelectedSeat: (state, action) => {
+      state.selectedSeat = action.payload;
+    },
+    setIsOpen: (state, action) => {
+      state.isOpen = action.payload;
+    },
+    setBooking: (state, action) => {
+      state.bookings.push(action.payload);
+    },
+    setCurrentUser:(state,action) => {
+      state.isCurrentUser = action.payload
+    }
+  },
+});
+
+export const { setCurrentDate, setSelectedSeat, setIsOpen, setBooking,setCurrentUser} =InitialSlice.actions;
+export default InitialSlice.reducer;
